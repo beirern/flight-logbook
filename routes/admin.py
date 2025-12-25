f58e44a4792c80e07db984e344b62d78315448d8
Nicola Beirer <nicolabeirer12@gmail.com>
2025-12-24 23:42:04 -0800

    Add new models for routes!

--- routes/admin.py
+++ routes/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from routes.models import Airport, Route, RouteWaypoint
+
+
 # Register your models here.
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "country", "municipality", "latitude", "longitude")
+    ordering = ["code"]
+
+
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+
+    def get_queryset(self, request):
+        # We tell Django: "When you fetch Routes, grab the waypoints too!"
+        qs = super().get_queryset(request)
+        return qs.prefetch_related("route_steps__waypoint")
+
+
+class RouteWaypointAdmin(admin.ModelAdmin):
+    list_display = ("route", "waypoint", "sequence")
+
+
+admin.site.register(Airport, AirportAdmin)
+admin.site.register(Route, RouteAdmin)
+admin.site.register(RouteWaypoint, RouteWaypointAdmin)
--- routes/models.py
+++ routes/models.py
@@ -3,8 +3,46 @@ from django.db import models
 
 # Create your models here.
 class Airport(models.Model):
-    code = models.TextField(max_length=4)
+    code = models.CharField(max_length=4)
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
+    country = models.CharField(max_length=2)
     municipality = models.TextField()
+
+    def __str__(self):
+        return f"{self.code} - {self.name}"
+
+
+class Route(models.Model):
+    name = models.TextField()
+    waypoints = models.ManyToManyField(
+        Airport, through="RouteWaypoint", related_name="flights_through"
+    )
+
+    def __str__(self):
+        # 1. Fetch the waypoint codes associated with this route in order
+        # We use 'route_steps' because that is the related_name on the RouteWaypoint model
+        stops = (
+            self.route_steps.all()
+            .order_by("sequence")
+            .values_list("waypoint__code", flat=True)
+        )
+
+        # 2. Join them: "KBFI -> KRNT -> KBFI"
+        path_string = " -> ".join(stops) if stops else "No waypoints assigned"
+
+        # 3. Return the name + the path
+        return f"{self.name}: ({path_string})"
+
+
+class RouteWaypoint(models.Model):
+    route = models.ForeignKey(
+        Route, on_delete=models.CASCADE, related_name="route_steps"
+    )
+    waypoint = models.ForeignKey(Airport, on_delete=models.CASCADE)
+    sequence = models.PositiveIntegerField()
+
+    class Meta:
+        ordering = ["sequence"]
+        unique_together = [["route", "sequence"]]
