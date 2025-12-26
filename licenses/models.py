360817a0c6f881f496fc49f0775594d1c2ea252d
Nicola Beirer <nicolabeirer12@gmail.com>
2025-12-25 21:47:10 -0800

    Actually use license types added earlier

--- licenses/models.py
+++ licenses/models.py
@@ -5,12 +5,12 @@ from pilots.models import Pilot
 
 class License(models.Model):
     class LicenseType(models.TextChoices):
-        PPL = "Private"
-        INS = "Instrument"
-        CFI = "Certified Flight Instructor"
-        CFII = "Certified Flight Instructor w/ Instrument"
+        PPL = "PPL", "Private Pilot"
+        INS = "INS", "Instrument"
+        CFI = "CFI", "Certified Flight Instructor"
+        CFII = "CFII", "Certified Flight Instructor w/ Instrument"
 
-    type = models.CharField(max_length=6)
+    type = models.CharField(max_length=6, choices=LicenseType.choices)
     pilot = models.ForeignKey(Pilot, on_delete=models.CASCADE, related_name="licenses")
     number = models.IntegerField()
     expiration = models.DateField(null=True, blank=True)
