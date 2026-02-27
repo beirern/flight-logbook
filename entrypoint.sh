04c4d3fb1e901d30302fb03a07b5daa53b52babe
Nicola Beirer <nicolabeirer12@gmail.com>
2026-02-26 23:42:01 -0800

    migrate to docker and pgsql

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# Wait for PostgreSQL
+until pg_isready -h "$POSTGRES_HOST" -U "$POSTGRES_USER"; do
+  echo "Waiting for postgres..."
+  sleep 1
+done
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+exec gunicorn flightlogbook.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 2
