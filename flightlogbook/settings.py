621511136873dca0ab70e00d57647c0496aeddd4
Nicola Beirer <nicolabeirer12@gmail.com>
2026-02-27 07:39:26 -0800

    Add CSRF domains

--- flightlogbook/settings.py
+++ flightlogbook/settings.py
@@ -26,6 +26,8 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",") if os.environ.get("CSRF_TRUSTED_ORIGINS") else []
+
 
 # Application definition
 
